9e0536302140a8511cc72117c57d0282757b1f50
DeveloperLaravelPHP <150107310+DeveloperLaravelPHP@users.noreply.github.com>
2025-07-03 18:13:18 +0530

    first 9 commit

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     domains: ["res.cloudinary.com"],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',// update with the domain you're loading from
-        port: '',
-        pathname: '/**',
+        protocol: "https", // no array
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
       },
-
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "",
+        pathname: "/**",
+      },
     ],
-
-  }
+  },
 };
 
 export default nextConfig;
