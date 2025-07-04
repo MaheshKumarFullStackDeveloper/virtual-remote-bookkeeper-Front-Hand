9ad33cc39599ea2ed779dfc4c460543c5a3b3bff
DeveloperLaravelPHP <150107310+DeveloperLaravelPHP@users.noreply.github.com>
2025-07-04 19:31:58 +0530

    51 commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import Footer from "./components/Footer";
 import LayoutWrapper from "./LayoutWrapper";
 import MainLoader from "@/lib/MainLoader";
 import { getHeaderFooterWidgets } from "./store/getHeaderFooterWidgets";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import "./globals.css";
 import "./style.css";
@@ -68,6 +69,7 @@ export default async function RootLayout({
             <Footer />
           </LayoutWrapper>
           <ToastContainer />
+          <SpeedInsights />
         </body>
       </html>
     </Suspense>
